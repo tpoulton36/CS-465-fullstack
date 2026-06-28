d4e03d576d09c0c0ad0676c6465c660fd35d114b
Tanner Poulton <tanner.poulton@snhu.edu>
2026-06-27 23:19:41 -0500

    Implement JWT authentication and secure API endpoints

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,16 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 
 const tripsController = require('../controllers/trips');
 const authenticationController = require('../controllers/authentication');
+
+function authenticateJWT(req, res, next) {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader) {
+    console.log('Auth Header Required but NOT PRESENT!');
+    return res.sendStatus(401);
+  }
+
+  const headers = authHeader.split(' ');
+
+  if (headers.length < 2) {
+    console.log('Not enough tokens in Auth Header');
+    return res.sendStatus(401);
+  }
+
+  const token = headers[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, verified) => {
+    if (err) {
+      console.log('Token Validation Error!');
+      return res.sendStatus(401);
+    }
+
+    req.auth = verified;
+    next();
+  });
+}
 
 router.post('/register', authenticationController.register);
 router.post('/login', authenticationController.login);
 
 router.get('/trips', tripsController.tripsList);
 router.get('/trips/:tripCode', tripsController.tripsFindCode);
-router.post('/trips', tripsController.tripsAddTrip);
-router.put('/trips/:tripCode', tripsController.tripsUpdateTrip);
-router.delete('/trips/:tripCode', tripsController.tripsDeleteTrip);
+router.post('/trips', authenticateJWT, tripsController.tripsAddTrip);
+router.put('/trips/:tripCode', authenticateJWT, tripsController.tripsUpdateTrip);
+router.delete('/trips/:tripCode', authenticateJWT, tripsController.tripsDeleteTrip);
 
 module.exports = router;
